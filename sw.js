8e4bf8a74f6eaa4dedc100e5b80e968aedf74cbe
JPMFernandes16 <iscap2250309@gmail.com>
2026-04-21 11:20:50 +0100

    Atualizar v8

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js
 // 🚨 REGRA DE OURO: SEMPRE QUE FIZERES ALTERAÇÕES NO PROJETO, MUDA ESTES NÚMEROS!
-const STATIC_CACHE = 'moodle-bia-static-v6'; 
-const DYNAMIC_CACHE = 'moodle-bia-dynamic-v6';
+const STATIC_CACHE = 'moodle-bia-static-v7'; 
+const DYNAMIC_CACHE = 'moodle-bia-dynamic-v7';
 
 // Apenas ficheiros da "carcaça" da aplicação (App Shell)
 // NOTA: Adicionei os novos ficheiros que criámos na refatoração!
